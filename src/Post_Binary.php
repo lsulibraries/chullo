988c229d4a3dece484740e1efea47b0b5811844d
flummingbird <wwconlin@gmail.com>
2016-02-10 16:28:48 -0600

    Fixed parent directory relationship. Now all children are only found in mingo5

--- src/Post_Binary.php
+++ src/Post_Binary.php
@@ -3,21 +3,22 @@ require '../../vendor/autoload.php';
 use Islandora\Chullo\Chullo;
 use GuzzleHttp\Client;
 
+//Create a parent folder called Mingo5 to conatin binaries...
 
 $chullo = Chullo::create("http://localhost:8080/fcrepo/rest");
 $uri = $chullo->createResource();
 $graph = $chullo->getGraph($uri);
-
-//give it your own title
 $graph->set($uri, 'dc:title', 'Mingo5');
 $chullo->saveGraph($uri, $graph);
 $transaction = $chullo->createTransaction();
+$chullo->commitTransaction($transaction);
 
 
-//$client = new Client(['base_uri' => $uri]);
 $client = new Client(['base_uri' => 'http://localhost:8080/fcrepo/rest']);
 
 //change $dirpath to the path of your folder of stuff (binaries)
+
+$children_transaction = $chullo->createTransaction();
 $dirpath = "/home/vagrant/islandora/Mingo5/";
 $dir = new DirectoryIterator($dirpath);
 
@@ -39,13 +40,13 @@ $child_uri = $chullo->createResource(
 
 
 $body = fopen($dirpath . $path, 'r');
-
+echo $child_uri . "\n";
 echo $body . "\n";
 //echo $dirpath . $path . "\n";
-$r = $client->request('POST', 'http://localhost:8080/fcrepo/rest', ['body' => $body]);
+$r = $client->request('POST', $uri, ['body' => $body]);
 }
 
-$chullo->commitTransaction($transaction);
+$chullo->commitTransaction($children_transaction);
 
 
  echo $uri . "\n";
