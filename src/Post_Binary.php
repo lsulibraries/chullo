0a0b3e30381425465efda6bc6fabfb43dc696815
flummingbird <wwconlin@gmail.com>
2016-02-10 16:46:54 -0600

    comments

--- src/Post_Binary.php
+++ src/Post_Binary.php
@@ -4,7 +4,6 @@ use Islandora\Chullo\Chullo;
 use GuzzleHttp\Client;
 
 //Create a parent folder called Mingo5 to conatin binaries...
-
 $chullo = Chullo::create("http://localhost:8080/fcrepo/rest");
 $uri = $chullo->createResource();
 $graph = $chullo->getGraph($uri);
@@ -14,10 +13,12 @@ $transaction = $chullo->createTransaction();
 $chullo->commitTransaction($transaction);
 
 
+//I want to add a layer here to take the 5010101 whatever and make it's own directory within mingo.. unless we want everything there flat in Mingo5 folder
+
+
+// guzzleHttp needs it's own client to reference.
 $client = new Client(['base_uri' => 'http://localhost:8080/fcrepo/rest']);
-
 //change $dirpath to the path of your folder of stuff (binaries)
-
 $children_transaction = $chullo->createTransaction();
 $dirpath = "/home/vagrant/islandora/Mingo5/";
 $dir = new DirectoryIterator($dirpath);
@@ -40,15 +41,11 @@ $child_uri = $chullo->createResource(
 
 
 $body = fopen($dirpath . $path, 'r');
-echo $child_uri . "\n";
-echo $body . "\n";
-//echo $dirpath . $path . "\n";
+// echo $child_uri . "\n";
+// echo $body . "\n";
 $r = $client->request('POST', $uri, ['body' => $body]);
 }
 
 $chullo->commitTransaction($children_transaction);
-
-
  echo $uri . "\n";
-
 ?>
